b34d13f56d228de2beb726c9d61d6b2c4e6b53b9
xkk-W <xw2667@nyu.edu>
2024-12-11 22:47:29 -0500

    Update checkLogin.php

--- database/checkLogin.php
+++ database/checkLogin.php
@@ -1,12 +1,10 @@
 <?php
 session_start();
+header('Content-Type: application/json');
 
-// Check if the user is logged in
-if (isset($_SESSION['user_id'])) {
-    // User is logged in
-    echo json_encode(["loggedIn" => true]);
+if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in']) {
+    echo json_encode(['loggedIn' => true]);
 } else {
-    // User is not logged in
-    echo json_encode(["loggedIn" => false]);
+    echo json_encode(['loggedIn' => false]);
 }
 ?>
