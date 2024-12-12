e03badff168b46d1711272d487bbc3c8dbba89bc
xkk-W <xw2667@nyu.edu>
2024-12-12 04:32:43 -0500

    Update savePet.php

--- petprofile/savePet.php
+++ petprofile/savePet.php
@@ -2,6 +2,10 @@
 include '../database/dbConnect.php';
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!isset($_SESSION['username'])) {
     echo json_encode(['success' => false, 'message' => 'User not logged in.']);
     exit;
