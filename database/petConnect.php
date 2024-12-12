f4d4f6cf94a9c5b18dbdd6c368bb9743ffd12579
xkk-W <xw2667@nyu.edu>
2024-12-12 08:22:35 -0500

    Update petConnect.php

--- database/petConnect.php
+++ database/petConnect.php
@@ -1,21 +1,16 @@
 <?php
-try {
+function petConnect() {
     $db = new PDO('sqlite:petcare.sqlite');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// Create table if it doesn't exist
-    $db->exec("
-        CREATE TABLE IF NOT EXISTS pets (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            type TEXT NOT NULL,
-            name TEXT NOT NULL,
-            age INTEGER NOT NULL,
-            history TEXT NOT NULL
-        );
-    ");
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    exit();
+    
+    // pet table
+    $db->exec("CREATE TABLE (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        type TEXT,
+        name TEXT,
+        age INTEGER,
+        history TEXT
+    )");
+    return $db;
 }
 ?>
