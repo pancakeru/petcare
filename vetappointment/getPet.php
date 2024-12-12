3469ccd04f41148e36f03e9905344584b17540d1
xkk-W <xw2667@nyu.edu>
2024-12-12 15:01:39 -0500

    Update getPet.php

--- vetappointment/getPet.php
+++ vetappointment/getPet.php
@@ -4,20 +4,23 @@ include '../database/petConnect.php';
 session_start();
 
 if (!isset($_SESSION['username'])) {
-    echo json_encode(["success" => false, "message" => "You must be logged in to view pets."]);
+    echo json_encode(["success" => false, "message" => "You must be logged in to view appointments."]);
     exit;
 }
 
 try {
     $username = $_SESSION['username'];
-    $stmt = $conn->prepare("SELECT * FROM Pets WHERE username = :username ORDER BY created_at DESC");
+
+    $stmt = $conn->prepare("SELECT a.id, a.date, a.time, a.reason, p.name AS petName, p.type AS petType FROM Appointments a INNER JOIN Pets p ON a.petId = p.id WHERE p.username = :username ORDER BY a.date ASC, a.time ASC");
     $stmt->bindValue(':username', $username, SQLITE3_TEXT);
     $result = $stmt->execute();
-    $pets = [];
+
+    $appointments = [];
     while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-        $pets[] = $row;
+        $appointments[] = $row;
     }
-    echo json_encode(["success" => true, "pets" => $pets]);
+
+    echo json_encode(["success" => true, "appointments" => $appointments]);
 } catch (Exception $e) {
     echo json_encode(["success" => false, "message" => $e->getMessage()]);
 }
