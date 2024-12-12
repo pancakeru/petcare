656e0c4ce7dcd082825c617fd4728d47ab1f0f8e
xkk-W <xw2667@nyu.edu>
2024-12-12 04:20:14 -0500

    Update deletePet.php

--- petprofile/deletePet.php
+++ petprofile/deletePet.php
@@ -2,17 +2,17 @@
 include '../database/dbConnect.php';
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['username'])) {
     echo json_encode(['success' => false, 'message' => 'User not logged in.']);
     exit;
 }
 
 $pet_id = $_POST['pet_id'];
 
-$sql = "DELETE FROM Pets WHERE id = ? AND user_id = ?";
+$sql = "DELETE FROM Pets WHERE id = ? AND username = ?";
 $stmt = $pdo->prepare($sql);
 
-if ($stmt->execute([$pet_id, $_SESSION['user_id']])) {
+if ($stmt->execute([$pet_id, $_SESSION['username']])) {
     echo json_encode(['success' => true, 'message' => 'Pet deleted successfully.']);
 } else {
     echo json_encode(['success' => false, 'message' => 'Error deleting pet.']);
