aefd8e52009a86aefc25b6d7a87eb2973f254018
xkk-W <xw2667@nyu.edu>
2024-12-11 22:09:05 -0500

    Update checkLogin.php

--- database/checkLogin.php
+++ database/checkLogin.php
@@ -3,7 +3,7 @@ session_start();
 header('Content-Type: application/json');
 
 $response = [
-    'loggedIn' => isset($_SESSION['user_id'])
+    'loggedIn' => isset($_SESSION['user_id'] && !empty($_SESSION['user_id'])
 ];
 
 echo json_encode($response);
