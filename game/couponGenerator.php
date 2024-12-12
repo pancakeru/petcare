da22d2483dacfab0bee10220665694cb8b148919
Zoe Chow <zoechowym22@gmail.com>
2024-12-12 17:44:03 -0500

    Update couponGenerator.php

--- game/couponGenerator.php
+++ game/couponGenerator.php
@@ -1,6 +1,10 @@
 <?php
 require_once '../database/dbConnect.php';
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
 try {
     $db = new SQLite3('../database/my_database.sqlite');
 
